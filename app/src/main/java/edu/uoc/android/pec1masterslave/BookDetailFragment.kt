af49a83df4af96d0136b92885abb7abb53f36795
lestelor <61620361+lestelor@users.noreply.github.com>
2020-10-06 14:43:26 +0200

    Full PEC1

--- app/src/main/java/edu/uoc/android/pec1masterslave/BookDetailFragment.kt
+++ app/src/main/java/edu/uoc/android/pec1masterslave/BookDetailFragment.kt
@@ -3,6 +3,7 @@ package edu.uoc.android.pec1masterslave
 
 import android.R.drawable
 import android.os.Bundle
+import android.text.Html
 import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,14 +11,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.ui.tooling.preview.Preview
 import edu.uoc.android.pec1masterslave.model.BookModel
+import java.io.File
 
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-private var item: String? = null
 
 /**
  * A simple [Fragment] subclass.
@@ -26,14 +38,14 @@ private var item: String? = null
  */
 class BookDetailFragment : Fragment() {
     // Rename and change types of parameters
-    private var item: BookModel.BookItem? = null
+    private lateinit var item: BookModel.BookItem
     private var param2: String? = null
 
     // get the parameters through the interface
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            item = arguments!!.getSerializable(BookDetailFragment.ARG_PARAM1) as BookModel.BookItem?
+            item = requireArguments().getSerializable(BookDetailFragment.ARG_PARAM1) as BookModel.BookItem
             param2 = it.getString(ARG_PARAM2)
         }
 
@@ -46,23 +58,59 @@ class BookDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         val fragmentView = inflater.inflate(R.layout.fragment_book_detail, container, false)
 
-        item?.let {
-            // create an integuer wich contains the associated jpg
-            val resID = resources.getIdentifier(
-                "cartel" + item!!.identificador.toString(),
-                "drawable",
-                activity?.packageName
-            )
-            // create view
-            val dateFormat = DateFormat.format("dd/MM/yyyy", item?.fechaPublicacion)
-            Log.d("cfauli", "drawable " + resID)
-            fragmentView.findViewById<ImageView>(R.id.fragment_book_image).setImageResource(resID)
-            fragmentView.findViewById<TextView>(R.id.fragment_book_author).text = item!!.autor
-            fragmentView.findViewById<TextView>(R.id.fragment_book_date).text = dateFormat
-            fragmentView.findViewById<TextView>(R.id.fragment_book_description).text = item!!.descripccion
+        item.let {
+
+
+            /* Here we substitute the layout by a jetpack-compose stile
+                fragmentView.findViewById<ImageView>(R.id.fragment_book_image).setImageResource(resID)
+                fragmentView.findViewById<TextView>(R.id.fragment_book_author).text = item!!.autor
+                fragmentView.findViewById<TextView>(R.id.fragment_book_date).text = dateFormat
+                fragmentView.findViewById<TextView>(R.id.fragment_book_description).text = item!!.descripccion
+                */
+
+
+            (fragmentView as ViewGroup).setContent {
+                Hello("Jetpack Compose", item)
+            }
         }
+
         return fragmentView
     }
+
+    @Composable
+    fun Hello(name: String, item:BookModel.BookItem) = MaterialTheme {
+        // create an integer which contains the associated jpg
+        val resID = resources.getIdentifier(
+            "cartel" + item.identificador.toString(),
+            "drawable",
+            activity?.packageName
+        )
+        // create view
+        val dateFormat = DateFormat.format("dd/MM/yyyy", item.fechaPublicacion)
+        Log.d("cfauli", "drawable " + resID)
+
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                val imageModifier = Modifier
+                //.preferredHeight(180.dp)
+                //.fillMaxWidth()
+
+                Image(
+                    imageResource(id = resID), modifier = imageModifier,
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(Modifier.preferredHeight(16.dp))
+                Text(item.descripccion!!)
+            }
+
+    }
+
+    /*@Preview
+    @Composable
+    fun DefaultPreview() {
+        Hello("Android", BookModel.BookItem(0))
+    }*/
 
     companion object {
         const val ARG_PARAM1 = "param1"
@@ -87,3 +135,7 @@ class BookDetailFragment : Fragment() {
     }
 
 }
+
+
+
+
