2dd3c988f5a94c6898d7e2fb4e5cd7002dc95764
lestelor <61620361+lestelor@users.noreply.github.com>
2020-10-08 09:30:05 +0200

    Full PEC1

--- app/src/main/java/edu/uoc/android/pec1masterslave/BookDetailFragment.kt
+++ app/src/main/java/edu/uoc/android/pec1masterslave/BookDetailFragment.kt
@@ -1,6 +1,7 @@
 package edu.uoc.android.pec1masterslave
 
 
+import android.app.Notification
 import android.graphics.Paint
 import android.os.Bundle
 import android.text.format.DateFormat
@@ -25,17 +26,22 @@ import androidx.compose.ui.Alignment.Companion.Bottom
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.ui.tooling.preview.Preview
 import edu.uoc.android.pec1masterslave.model.BookModel
-import java.time.format.TextStyle
+
 import java.util.*
 
 
@@ -67,7 +73,7 @@ class BookDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val fragmentView = inflater.inflate(R.layout.fragment_book_detail, container, false)
+        //val fragmentView = inflater.inflate(R.layout.fragment_book_detail, container, false)
 
         item.let {
 
@@ -79,18 +85,25 @@ class BookDetailFragment : Fragment() {
                 fragmentView.findViewById<TextView>(R.id.fragment_book_description).text = item!!.descripccion
                 */
 
-
-            (fragmentView as ViewGroup).setContent {
-                Hello(item)
+            return ComposeView(requireContext()).apply {
+                setContent {
+                    Hello(item)
+                }
             }
+
+            /*(fragmentView as ViewGroup).setContent {
+                Hello(item)
+            }*/
         }
 
-        return fragmentView
+        //return fragmentView
     }
 
     @Composable
     fun Hello(item: BookModel.BookItem) = MaterialTheme {
         // create an integer which contains the associated jpg
+        val setPadding = 16.dp
+
         val resID = resources.getIdentifier(
             "cartel" + item.identificador.toString(),
             "drawable",
@@ -99,7 +112,6 @@ class BookDetailFragment : Fragment() {
         val dateFormat = DateFormat.format("dd/MM/yyyy", item.fechaPublicacion)
 
         Log.d("cfauli", "drawable " + resID)
-
 
         ConstraintLayout() {
             val (image, column, text, text2) = createRefs()
@@ -115,19 +127,19 @@ class BookDetailFragment : Fragment() {
             )
 
             Column(Modifier
-                .padding(16.dp)
+                .padding(setPadding)
                 .constrainAs(column) {
                     start.linkTo(image.end)
                     top.linkTo(parent.top)
                 }) {
-                Text(item.titulo.toString())
+                Text(item.titulo.toString(), style = MaterialTheme.typography.h1)
                 Text(item.autor.toString())
                 Text(item.urlPortada.toString())
             }
 
             Text(
                 modifier = Modifier
-                    .padding(16.dp)
+                    .padding(setPadding)
                     .constrainAs(text) {
                         start.linkTo(image.end)
                         bottom.linkTo(image.bottom)
@@ -137,43 +149,24 @@ class BookDetailFragment : Fragment() {
 
             Text(
                 modifier = Modifier
-                    .padding(16.dp)
+                    .padding(setPadding)
                     .constrainAs(text2) {
                         start.linkTo(parent.start)
                         top.linkTo(image.bottom)
                     },
-                text = item.descripccion.toString()
+                text = item.descripccion.toString(),
+                style = MaterialTheme.typography.body1
             )
 
         }
+
 
     }
 
     @Preview ("BookDetailFragment")
     @Composable
     fun Preview() {
-        val resID = resources.getIdentifier(
-            "cartel" + BookModel.book1.identificador.toString(),
-            "drawable",
-            activity?.packageName
-        )
-        ConstraintLayout(
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            val(image) = createRefs()
-            Column(){
-                Row(Modifier.fillMaxWidth()) {
-                    Image(
-                        imageResource(R.drawable.cartel0),
-                        contentScale = ContentScale.Crop
-                    )
-                    Box(Modifier.fillMaxSize()) {
-                        Text("Casdfsdf")
-                        Text("sfdfsd")
-                    }
-                }
-            }
-        }
+       Hello(BookModel.book1)
     }
 
     @Preview ("BookDetailFragment")
@@ -230,6 +223,7 @@ class BookDetailFragment : Fragment() {
     companion object {
         const val ARG_PARAM1 = "param1"
         const val ARG_PARAM2 = "param2"
+
         /* Use this factory method to create a new instance of
         * this fragment using the provided parameters.
         *
